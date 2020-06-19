d6a442fc94cb952e868b99a36b229ba5b3340e10
yashleela <52657140+yashleela@users.noreply.github.com>
2020-06-19 07:52:10 +0530

    Add files via upload

---
+++ Assignment1_square.py
@@ -0,0 +1,8 @@
+print("Enter 'x' for exit.");
+side = input("Enter side length of square: ");
+if side == 'x':
+    exit();
+else:
+    slength = int(side);
+    perimeter = 4*slength;
+    print("\nPerimeter of Square =", perimeter);
