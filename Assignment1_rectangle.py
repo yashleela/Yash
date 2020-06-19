ac5c678ca455fce3b2d7508f29e8ad8ade3671c6
yashleela <52657140+yashleela@users.noreply.github.com>
2020-06-19 07:53:15 +0530

    Add files via upload

---
+++ Assignment1_rectangle.py
@@ -0,0 +1,10 @@
+width = float(input('Please Enter the Width of a Rectangle: '))
+height = float(input('Please Enter the Height of a Rectangle: '))
+
+Area = width * height
+
+
+Perimeter = 2 * (width + height)
+
+print("\n Area of a Rectangle is: %.2f" %Area)
+print(" Perimeter of Rectangle is: %.2f" %Perimeter)
